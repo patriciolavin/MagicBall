99690d774cee2a5a480c418388abb4f189974a6c
patriciolavin <plavinp@gmail.com>
2021-05-16 21:21:02 -0400

    MagicBall v.3.2 - Final [w/ background changing colors]

--- app/src/main/java/com/example/magicball/MainActivity.java
+++ app/src/main/java/com/example/magicball/MainActivity.java
@@ -7,17 +7,22 @@ import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    ImageView imageView1,imageView2;
+
     private ConstraintLayout layout;
-    private Button btn;
+    private ImageButton btn;
 
     private ImageButton ball;
     private TextView respuesta;
@@ -25,24 +30,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
 
-    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-/*        layout=findViewById(R.id.layoutt);
-        ball=findViewById(R.id.btn);
+        //bloques superior e inferior de cambio de color
+        imageView1 = findViewById(R.id.image_view1);
+        imageView2 = findViewById(R.id.image_view2);
 
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Random random = new Random();
-
-                int color = Color.argb(255, random.nextInt(256), random.nextInt(256), random.nextInt(256));
-                layout.setBackgroundColor(color);
-        }
-        });*/
+        //establece el timer de cambio del fondo
+        Timer timer = new Timer();
+        MyTimer myTimer = new MyTimer();
+        timer.schedule(myTimer, 2000, 2000);
 
 
         //se referencia la imagen
@@ -54,17 +54,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //proceso
         ball.setOnClickListener(this);
         Toast.makeText(MainActivity.this, "Consulta tu Suerte", Toast.LENGTH_SHORT).show();
-    }
+
+    } // fin de protected void onCreate(Bundle savedInstanceState)
+
+
+    //función de cambio dinámico y aleatorio (random) del fondo en las zonas definidas
+    private class MyTimer extends TimerTask {
+
+        @Override
+        public void run() {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Random random = new Random();
+                    //set color background bloque superior
+                    imageView1.setBackgroundColor(Color.argb(255, random.nextInt(256), random.nextInt(256), random.nextInt(256)));
+
+                    //set color background bloque inferior
+                    imageView2.setBackgroundColor(Color.argb(255, random.nextInt(256), random.nextInt(256), random.nextInt(256)));
+                }
+            });
+        }
+    } // fin de private class MyTimer
 
 
     @Override
         public void onClick(View v){
-        //creación método switch
+
+        //creación método switch e invocación de los elementos del array de respuestas
             switch (v.getId()){
                 case R.id.btn:
                     int rand = new Random().nextInt(respuestaArray.length);
                     respuesta.setText(respuestaArray[rand]);
             }
         }
-}
+} // fin de la public class MainActivity
 
